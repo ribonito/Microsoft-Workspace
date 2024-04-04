be9f5fb70cff85684ed2ab6c60938114d6d10d1b
artem.mossitchev <artem.mossitchev@sunrise.net>
2024-04-04 16:55:47 +0200

    update

--- Intune_Scripts/InstallPSModules.ps1
+++ Intune_Scripts/InstallPSModules.ps1
@@ -9,4 +9,5 @@ Install-Module -name Microsoft.Graph -Force
 Install-Module -name Microsoft.Graph.beta -Force
 Install-Module -Name MSGraphFunctions -Force
 Install-Module -Name IntuneBackupAndRestore -Force
+Install-Module -Name WindowsAutopilotPartnerCenter –force
 
