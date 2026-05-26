d6d491fbf9731bd3afb47a00e9e9cb97b790577c
Josep Canas <josep@canyas.ch>
2026-05-26 16:42:59 +0200

    Changes

--- Mailboxtypes.ps1
+++
@@ -1,6 +0,0 @@
-Connect-ExchangeOnline
-
-#Enable remote mailboxes in Exo with PS from a csv file 
-Import-Csv -Path "C:\Users\canasmarin\downloads\enable_remote.csv" | ForEach-Object {
-    Enable-RemoteMailbox -Identity $_.samaccountname -RemoteRoutingAddress $_.userprincipalname -Shared
-}
