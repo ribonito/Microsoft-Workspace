4d1fdc73fcf7996cb0886e76e4432998d270d87a
jcany <jcany@ribonito.com>
2026-06-07 15:55:28 +0200

    refactor: remove unused project configuration files and associated utility scripts

--- OnPremises_Scripts/Intune/OD-MountTimer-remediate.ps1
+++
@@ -1,6 +0,0 @@
-﻿$Path = "HKCU:\SOFTWARE\Microsoft\OneDrive\Accounts\Business1"
-$Name = "Timerautomount"
-$Type = "QWORD"
-$Value = 1
-
-Set-ItemProperty -Path $Path -Name $Name -Type $Type -Value $Value 
